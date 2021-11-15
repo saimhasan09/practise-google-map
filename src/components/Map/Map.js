f42e3df5102a671f6db2d2f1803a6f5d13dc84c3
saimhasan09 <saimhasannahid@gmail.com>
2021-11-15 12:21:00 +0600

    m-62-5-1

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+};
+
+const center = {
+    lat: 22.845640,
+    lng: 89.540329
+};
 
 const Map = () => {
     return (
-        <div>
-            <h1>My Map</h1>
-        </div>
+        <LoadScript
+            googleMapsApiKey="YOUR_API_KEY"
+        >
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+            >
+                { /* Child components, such as markers, info windows, etc. */}
+                <></>
+            </GoogleMap>
+        </LoadScript>
     );
 };
 
